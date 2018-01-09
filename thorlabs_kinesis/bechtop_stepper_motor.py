1ac23152fabc7f7e4faf7fd2bdd97f14e1e6e4ca
Ertugrul Karademir <ekarademir@gmail.com>
2018-01-09 13:00:12 +0000

    enums as types

--- thorlabs_kinesis/bechtop_stepper_motor.py
+++ thorlabs_kinesis/bechtop_stepper_motor.py
@@ -28,6 +28,7 @@ FT_InsufficientResources = c_short(0x05)
 FT_InvalidParameter = c_short(0x06)
 FT_DeviceNotPresent = c_short(0x07)
 FT_IncorrectDevice = c_short(0x08)
+FT_Status = c_short
 
 # enum MOT_MotorTypes
 MOT_NotMotor = c_int(0)
@@ -35,40 +36,48 @@ MOT_DCMotor = c_int(1)
 MOT_StepperMotor = c_int(2)
 MOT_BrushlessMotor = c_int(3)
 MOT_CustomMotor = c_int(100)
+MOT_MotorTypes = c_int
 
 # enum MOT_TravelModes
 MOT_TravelModeUndefined = c_int(0x00)
 MOT_Linear = c_int(0x01)
 MOT_Rotational = c_int(0x02)
+MOT_TravelModes = c_int
 
 # enum MOT_TravelDirection
 MOT_TravelDirectionUndefined = c_short(0x00)
 MOT_Forwards = c_short(0x01)
 MOT_Reverse = c_short(0x02)
+MOT_TravelDirection = c_short
 
 # enum MOT_HomeLimitSwitchDirection
 MOT_LimitSwitchDirectionUndefined = c_short(0x00)
 MOT_ReverseLimitSwitch = c_short(0x01)
 MOT_ForwardLimitSwitch = c_short(0x04)
+MOT_HomeLimitSwitchDirection = c_short
 
 # enum MOT_DirectionSense
 MOT_Normal = c_short(0x00)
 MOT_Backwards = c_short(0x01)
+MOT_DirectionSense = c_short
 
 # enum MOT_JogModes
 MOT_JogModeUndefined = c_short(0x00)
 MOT_Continuous = c_short(0x01)
 MOT_SingleStep = c_short(0x02)
+MOT_JogModes = c_short
 
 # enum MOT_StopModes
 MOT_StopModeUndefined = c_short(0x00)
 MOT_Immediate = c_short(0x01)
 MOT_Profiled = c_short(0x02)
+MOT_StopModes = c_short
 
 # enum MOT_ButtonModes
 MOT_ButtonModeUndefined = c_word(0x00)
 MOT_JogMode = c_word(0x01)
 MOT_Preset = c_word(0x02)
+MOT_ButtonModes = c_word
 
 # enum MOT_LimitSwitchModes
 MOT_LimitSwitchModeUndefined = c_word(0x00)
@@ -83,6 +92,7 @@ MOT_LimitSwitchMakeOnContactSwapped = c_word(0x82)
 MOT_LimitSwitchBreakOnContactSwapped = c_word(0x83)
 MOT_LimitSwitchMakeOnHomeSwapped = c_word(0x84)
 MOT_LimitSwitchBreakOnHomeSwapped = c_word(0x85)
+MOT_LimitSwitchModes = c_word
 
 # enum MOT_LimitSwitchSWModes
 MOT_LimitSwitchSWModeUndefined = c_word(0x00)
@@ -92,26 +102,31 @@ MOT_LimitSwitchStopProfiled = c_word(0x03)
 MOT_LimitSwitchIgnored_Rotational = c_word(0x81)
 MOT_LimitSwitchStopImmediate_Rotational = c_word(0x82)
 MOT_LimitSwitchStopProfiled_Rotational = c_word(0x83)
+MOT_LimitSwitchSWModes = c_word
 
 # enum MOT_LimitsSoftwareApproachPolicy
 DisallowIllegalMoves = c_int16(0)
 AllowPartialMoves = c_int16(1)
 AllowAllMoves = c_int16(2)
+MOT_LimitsSoftwareApproachPolicy = c_int16
 
 # enum MOT_PID_LoopMode
 MOT_PIDLoopModeDisabled = c_word(0x00)
 MOT_PIDOpenLoopMode = c_word(0x01)
 MOT_PIDClosedLoopMode = c_word(0x02)
+MOT_PID_LoopMode = c_word
 
 # enum MOT_MovementModes
 LinearRange = c_int(0x00)
 RotationalUnlimited = c_int(0x01)
 RotationalWrapping = c_int(0x02)
+MOT_MovementModes = c_int
 
 # enum MOT_MovementDirections
 Quickest = c_int(0x00)
 Forwards = c_int(0x01)
 Reverse = c_int(0x02)
+MOT_MovementDirections = c_int
 
 
 class TLI_DeviceInfo(Structure):
