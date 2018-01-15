b6388422ec02fee8892859f103f0f3b4b22c770a
Ertugrul Karademir <ekarademir@gmail.com>
2018-01-15 16:10:28 +0000

    expanded init

--- thorlabs_kinesis/ext/__init__.py
+++ thorlabs_kinesis/ext/__init__.py
@@ -1,9 +1,13 @@
 from thorlabs_kinesis.ext._device import (
     serial_prefix,
     expand_device,
+    device_to_real_units,
+    real_to_device_units,
 )
 
 __all__ = [
     "serial_prefix",
     "expand_device",
+    "device_to_real_units",
+    "real_to_device_units",
 ]
