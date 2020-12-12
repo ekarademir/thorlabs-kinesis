a76cec27ab8892a165c27bd663ccaff6172dbd72
David Andrew Hill <dah3141@et.byu.edu>
2020-12-12 10:35:50 -0700

    updated functions, added AOI support

--- thorlabs_kinesis/thor_camera.py
+++ thorlabs_kinesis/thor_camera.py
@@ -27,21 +27,25 @@ class UC480_CAMERA_LIST(Structure):
     _fields_ = [("dwCount", c_int),
                 ("uci", UC480_CAMERA_INFO * 1)]
 
+class IS_2D(Structure):
+    _fields_ = [('s32X', c_int), ('s32Y', c_int)]
+
 GetNumberOfCameras = bind(lib, "is_GetNumberOfCameras", [POINTER(c_int)], c_int)
 GetCameraList = bind(lib, "is_GetCameraList", [POINTER(UC480_CAMERA_LIST)], c_int)
 
-InitCamera = bind(lib, "is_InitCamera", [POINTER(c_int), POINTER(c_int)], c_int)
+InitCamera = bind(lib, "is_InitCamera", [POINTER(c_int)], c_int)
 SetDisplayMode = bind(lib, "is_SetDisplayMode", [POINTER(c_int), c_int], c_int)
-PixelClock = bind(lib, "is_PixelClock", [POINTER(c_int), c_int, POINTER(c_uint), c_uint], c_int)
-SetColorMode = bind(lib, "is_SetColorMode", [POINTER(c_int), c_int], c_int)
-SetExposure = bind(lib, "is_Exposure", [POITNER(c_int), 12, POITNER(c_double), c_uint], c_int)
-SetFrameRate = bind(lib, "is_SetFrameRate", [POITNER(c_int), c_double, POINTER(c_double)])
+PixelClock = bind(lib, "is_PixelClock", [c_int, c_uint, POINTER(c_uint), c_uint], c_int)
+SetColorMode = bind(lib, "is_SetColorMode", [c_int, c_int], c_int)
+SetExposure = bind(lib, "is_Exposure", [c_int, c_uint, POINTER(c_double), c_uint], c_int)
+SetFrameRate = bind(lib, "is_SetFrameRate", [c_int, c_double, POINTER(c_double)])
+AOI = bind(lib, "is_AOI", [c_int, c_uint, POINTER(IS_2D), c_uint], c_int)
 
-FreeMemory = bind(lib, "is_FreeImageMem", [POITNER(c_int), POINTER(c_char), c_int], c_int)
-AllocateMemory = bind(lib, "is_SetAllocatedImageMem", [POINTER(c_int), c_int, c_int, c_int, POINTER(c_char), POITNER(c_int)], c_int)
-SetImageMemory = bind(lib, "is_SetImageMem", [POINTER(c_int), POITNER(c_char), c_int], c_int)
+FreeMemory = bind(lib, "is_FreeImageMem", [c_int, POINTER(c_ubyte * 1310720), c_int], c_int)
+AllocateMemory = bind(lib, "is_SetAllocatedImageMem", [c_int, c_int, c_int, c_int, c_ubyte * 1310720, POINTER(c_int)], c_int)
+SetImageMemory = bind(lib, "is_SetImageMem", [c_int, c_ubyte * 1310720, c_int], c_int)
 
-StartCapture = bind(lib, "is_CaptureVideo", [POINTER(c_int), c_int], c_int)
-StopCapture = bind(lib, "is_StopLiveVideo", [POITNER(c_int), c_int], c_int)
+StartCapture = bind(lib, "is_CaptureVideo", [c_int, c_int], c_int)
+StopCapture = bind(lib, "is_StopLiveVideo", [c_int, c_int], c_int)
 
-ExitCamera = bind(lib, "is_ExitCamera", [POITNER(c_int)], c_int)
+ExitCamera = bind(lib, "is_ExitCamera", [c_int], c_int)
